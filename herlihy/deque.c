1797a98e21135c510bd2824e0726b839ecb8673a
Sean Brennan <whalesonstilts2012@gmail.com>
2013-04-03 22:09:17 -0400

    oh man so much stuff happened. it was crazy.

---
+++ herlihy/deque.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+#include "deque.h"
+
+int main(int argc, char *argv[]) {
+    printf("LNULL: %d, RNULL: %d, DNULL: %d\n", LNULL, RNULL, DNULL);
+    return 0;
+}
---
+++ herlihy/deque.h
@@ -0,0 +1,4 @@
+
+#define LNULL 0b01
+#define RNULL 0b10
+#define DNULL 0b11 
