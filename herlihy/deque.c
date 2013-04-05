5de4b27c8633343fefad1f136dd192651ce4403e
Sean Brennan <whalesonstilts2012@gmail.com>
2013-04-05 01:00:29 -0400

    fucking hell yes

--- herlihy/deque.c
+++ herlihy/deque.c
@@ -2,6 +2,43 @@
 #include <stdio.h>
 #include "deque.h"
 
+
+void left_push(bounded_queue_t *deque, double *elt) {
+    // fill in later
+}
+
+double *left_pop(bounded_queue_t *deque) {
+    // fill in later
+
+}
+
+unsigned long long int left_checked_oracle(bounded_queue_t *deque) {
+    // fill in later
+
+}
+
+void right_push(bounded_queue_t *deque, double *elt) {
+    // fill in later
+
+}
+
+double *right_pop(bounded_queue_t *deque) {
+    // fill in later
+
+}
+
+unsigned long long int right_checked_oracle(bounded_queue_t *deque) {
+    // fill in later
+    while(1) {
+        
+    }
+}
+
+unsigned long long int oracle(oracle_ends end) {
+    // fill in later
+    
+}
+
 int main(int argc, char *argv[]) {
     printf("LNULL: %d, RNULL: %d, DNULL: %d\n", LNULL, RNULL, DNULL);
     return 0;
--- herlihy/deque.h
+++ herlihy/deque.h
@@ -2,3 +2,33 @@
 #define LNULL 0b01
 #define RNULL 0b10
 #define DNULL 0b11 
+#define DEF_BOUNDS 4096
+
+/*
+ * Types:
+ *     ...
+ */
+
+typedef struct bounded_deque_node_struct {
+    double *value;
+} bounded_deque_node_t;
+
+typedef struct bounded_deque_struct {
+    bounded_deque_node_t nodes[DEF_BOUNDS];
+    unsigned long long int size;
+    // ...
+} bounded_deque_t;
+
+typedef enum { LEFT, RIGHT } oracle_ends;
+
+/*
+ * Function Prototypes
+ */
+
+void left_push(bounded_queue_t *deque, double *elt);
+double *left_pop(bounded_queue_t *deque);
+unsigned long long int left_checked_oracle(bounded_queue_t *deque);
+void right_push(bounded_queue_t *deque, double *elt);
+double *right_pop(bounded_queue_t *deque);
+unsigned long long int right_checked_oracle(bounded_queue_t *deque);
+unsigned long long int oracle(oracle_ends end);
