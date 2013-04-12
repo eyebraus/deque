c44419226209ed7015502f67f70c1ca7ac35f2c1
Sean Brennan <whalesonstilts2012@gmail.com>
2013-04-11 23:54:03 -0400

    save state

---
+++ herlihy/deque_simple.h
@@ -0,0 +1,55 @@
+
+#define LNULL (int *) 0b01
+#define RNULL (int *) 0b10
+#define DEF_BOUNDS 4096
+#define CAS_NODE(p,o,n) (__sync_bool_compare_and_swap((long long *) (p), *(long long *) &(o), *(long long *) &(n)))
+#define ATOMIC_EQL(p,n) CAS_NODE(p,n,n)
+#define IS_NULL(v) ((v) == NULL || (v) == LNULL || (v) == RNULL || (v) == DNULL)
+#define VAL_EQL(a,b) ((a)->value == (b)->value)
+
+/*
+ * Types:
+ *     ...
+ */
+
+typedef struct bounded_deque_node_struct {
+    int *value;
+    unsigned int count;
+} bounded_deque_node_t;
+
+typedef struct bounded_deque_struct {
+    bounded_deque_node_t nodes[DEF_BOUNDS];
+    unsigned long long int size;
+    // ...
+} bounded_deque_t;
+
+typedef struct oracle_result_struct {
+    unsigned long long int k;
+    bounded_deque_node_t left;
+    bounded_deque_node_t right;
+} oracle_result_t;
+
+typedef enum { LEFT, RIGHT } oracle_ends;
+typedef enum { OK, EMPTY, FULL } deque_state;
+
+/*
+ * Function Prototypes
+ */
+
+// stack ops
+void left_push(bounded_deque_t *deque, int elt, int *stat);
+int left_pop(bounded_deque_t *deque, int *stat);
+void right_push(bounded_deque_t *deque, int elt, int *stat);
+int right_pop(bounded_deque_t *deque, int *stat);
+unsigned long long int oracle(oracle_ends end);
+
+// various helpers
+void init_bounded_deque_node(bounded_deque_node_t *node);
+void init_bounded_deque(bounded_deque_t *deque);
+void init_oracle_result(oracle_result_t *result);
+void clear_bounded_deque_node(bounded_deque_node_t *node);
+void clear_bounded_deque(bounded_deque_t *deque);
+void clear_oracle_result(oracle_result_t *result);
+void set_bounded_deque_node(bounded_deque_node_t *node, int *value, unsigned int last_count);
+void copy_bounded_deque_node(bounded_deque_node_t *new_node, bounded_deque_node_t *old_node);
+
