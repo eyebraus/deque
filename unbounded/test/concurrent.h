eb89c1baca5218fd3ea337fa2b42c6c3c4bd3073
Sean Brennan <whalesonstilts2012@gmail.com>
2013-04-28 23:45:55 -0400

    finish up thread pool work and write concurrent specs

--- unbounded/test/concurrent.h
+++ unbounded/test/concurrent.h
@@ -6,9 +6,25 @@ typedef struct thread_args_struct {
     pthread_barrier_t *barrier;
 } thread_args_t;
 
+typedef struct thread_pool_struct {
+    int id;
+    int size;
+    pthread_t *threads;
+} thread_pool_t;
+
+static inline void init_thread_pool(thread_pool_t &pool, int id, int size, thread_args_t *args) {
+    pool.id = id;
+    pool.size = size;
+    pool.threads = (pthread_t *) malloc(sizeof(pthread_t) * size);
+}
+
 static inline void init_thread_args(thread_args_t &args, unsigned int id, pthread_barrier_t *barrier) {
     args.id = id;
     args.barrier = barrier;
+}
+
+static inline void start_thread_pool(thread_pool_t &pool, void *(*proc)(void *), thread_args_t &args) {
+    
 }
 
 static inline void start_pthread(pthread_t &thread, void *(*proc)(void *), thread_args_t &args) {
