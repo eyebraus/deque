2fe6382cb4350fab7cf70015fccabb78c656d2ba
Sean Brennan <whalesonstilts2012@gmail.com>
2013-05-09 03:52:38 -0400

    results, collection scripts

---
+++ unbounded/exp/collect_mutex.sh
@@ -0,0 +1,26 @@
+
+for t in 2 4 8 16 32 64
+do
+    for w in "stack" "queue" "random"
+    do
+        for i in 1 2 3 4 5
+        do
+            echo iteration $t:timing:$w:$i
+            exp/mutex -t $t -e timing -w $w -o 5000 > exp/results/mutex_timing_"$w"_"$t"_"$i".out
+            sleep 2
+        done
+    done
+done
+
+for t in 2 4 8 16 32 64
+do
+    for w in "stack" "queue" "random"
+    do
+        for i in 1 2 3 4 5
+        do
+            echo iteration $t:throughput:$w:$i
+            exp/mutex -t $t -e throughput -w $w -o 2 > exp/results/mutex_throughput_"$w"_"$t"_"$i".out
+            sleep 2
+        done
+    done
+done
